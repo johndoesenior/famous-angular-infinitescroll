8b468861bd1ca16689e5fd406972aa094f2d9676
talgleichger <tal@godisco.net>
2014-11-01 15:06:38 +0200

    added basic functionallity for infinite scrolling

--- faInfiniteScroll.js
+++ faInfiniteScroll.js
@@ -14,12 +14,65 @@ angular.module('faInfiniteScroll',[
         return {
             restrict: 'EA',
             template: '',
-            scope: {todo: '&', when: '=', height: '='},
+            scope: {todo: '&', when: '=', height: '=', length: '@'},
             controllerAs: 'infinteScroll',
             controller: function($scope) {
                 var infiniteScroll = this;
                 var Transitionable = $famous['famous/transitions/Transitionable'];
                 var Timer = $famous['famous/utilities/Timer'];
+
+                var totalHeight = 0;
+                var when = $scope.when.replace('%','') / 100; // calculate percentage
+                var oldLength = 0;
+                var currentLength = _.clone($scope.length);
+
+                infiniteScroll.inProgress = false;
+                infiniteScroll.page = 0;
+
+                /**
+                 * Caching the render node
+                 * @Author: Avi Haiat
+                 * @param cacheEl
+                 * @param findSelector
+                 * @returns {*}
+                 */
+                var getRenderNode = function(cacheEl, findSelector) {
+                    if(!angular.isDefined(cacheEl)) {
+                        var el = $famous.find(findSelector)[0];
+                        if(el) {
+                            cacheEl = el.renderNode;
+                        }
+                    }
+                    return cacheEl;
+                };
+
+                /**
+                 * Get the scroll view position
+                 * @returns float
+                 */
+                var getScrollView = function() {
+                    infiniteScroll.scrollview = getRenderNode(infiniteScroll.scrollview, '#scrollView');
+                    return infiniteScroll.scrollview;
+                };
+
+                /**
+                 * Check for overpulls
+                 */
+                Timer.every(function() {
+                    totalHeight = $scope.height * $scope.length;
+                    infiniteScroll.when = -((totalHeight * when) - totalHeight);
+                    if(getScrollView().getAbsolutePosition() >= infiniteScroll.when && !infiniteScroll.inProgress && oldLength != currentLength) {
+                        infiniteScroll.page++;
+                        infiniteScroll.inProgress = true;
+                        $scope.todo(); // infinte scroll done!
+                    }
+                }, 2);
+
+                $scope.$on("infiniteScroll.done", function(){
+                    oldLength = _.clone(currentLength);
+                    currentLength = _.clone($scope.length);
+                    infiniteScroll.inProgress = false;
+                });
             }
         }
     });
